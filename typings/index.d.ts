43ce8775832c7148091689b80ecebc1c9fc1aee8
Chao <zhangchao@hsort.com>
2021-08-10 13:31:01 +0800

    1

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,14 +2,11 @@
 /// <reference path="./payment/index.d.ts" />
 /// <reference path="./webview/index.d.ts" />
 
-export = plus;
-export as namespace plus;
-
-declare const _: plus.PlusStatic;
+declare const plus: plus.PlusStatic;
 
 declare namespace plus {
   /**
    * https://www.dcloud.io/docs/api/index.html
    */
-  interface PlusStatic {}
+  interface PlusStatic { }
 }
