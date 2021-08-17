0b410fa34775a8df96e59c166874d0a8e1f27b61
Chao <zhangchao@hsort.com>
2021-08-17 09:54:55 +0800

    1

---
+++ typings/webview/WebviewObject.d.ts
@@ -0,0 +1,42 @@
+/// <reference path="./index.d.ts" />
+
+declare namespace plus.webview {
+  interface WebviewObject {
+    /**
+     * Webview窗口的标识
+     * 调用plus.webview.create或plus.webview.open新建窗口时传入的id参数值，如果没有设置id参数，此属性值为undefined。 注意：窗口标识只能在创建时设置，不支持动态修改，不要对此属性进行赋值操作。
+     */
+    readonly id: string;
+
+    /**
+     * 获取当前Webview窗口的创建者
+     */
+    opener(): WebviewObject;
+
+    show(aniShow?: any, duration?: number, showedCB?: Function, extras?: any): void;
+
+    /**
+     * 隐藏Webview窗口可保存已加载HTML页面的上下文数据，能降低应用使用的系统资源，通过show方法可将隐藏的Webview窗口显示出来。
+     * @param aniHide
+     * @param duration
+     * @param extras
+     */
+    hide(aniHide?: AnimationTypeClose, duration?: number, extras?: any): void;
+
+    /**
+     * 关闭并销毁Webview窗口，可设置关闭动画和动画持续时间。
+     * @param aniClose 关闭Webview窗口动画效果
+     * @param duration 关闭Webview窗口的动画持续时间
+     * @param extras 关闭Webview窗口扩展参数
+     */
+    close(aniClose?: AnimationTypeClose, duration?: number, extras?: any): void;
+
+    /**
+     * 将JS脚本发送到Webview窗口中运行，可用于实现Webview窗口间的数据通讯。
+     * @param js 要在窗口中运行的脚本字符串
+     */
+    evalJS(js: string): void;
+
+    [key: string]: any;
+  }
+}
--- typings/webview/index.d.ts
+++ typings/webview/index.d.ts
@@ -1,3 +1,5 @@
+/// <reference path="./WebviewObject.d.ts" />
+
 declare namespace plus {
   interface PlusStatic {
     /**
@@ -15,6 +17,13 @@ declare namespace plus {
        */
       close(id_wvobj: string | plus.webview.WebviewObject, aniClose?: plus.webview.AnimationTypeClose, duration?: number, extras?: any): void;
 
+      /**
+       * 创建Webview窗口，用于加载新的HTML页面，可通过styles设置Webview窗口的样式，创建完成后需要调用show方法才能将Webview窗口显示出来。
+       * @param url 打开窗口加载的HTML页面地址 新打开Webview窗口要加载的HTML页面地址，可支持本地地址和网络地址。
+       * @param id 打开窗口的标识 窗口标识可用于在其它页面中通过getWebviewById来查找指定的窗口，为了保持窗口标识的唯一性，应该避免使用相同的标识来创建多个Webview窗口。 如果传入无效的字符串则使用url参数作为WebviewObject窗口的id值。
+       * @param styles 创建Webview窗口的样式（如窗口宽、高、位置等信息）
+       * @param extras 创建Webview窗口的额外扩展参数 值为JSON类型，设置扩展参数后可以直接通过Webview的点（“.”）操作符获取扩展参数属性值，如： var w=plus.webview.create('url.html','id',{},{preload:'preload webview'}); // 可直接通过以下方法获取preload值 console.log(w.preload); // 输出值为“preload webview”
+       */
       create(url?: string, id?: string, styles?: any, extras?: object): plus.webview.WebviewObject;
 
       /**
@@ -47,13 +56,24 @@ declare namespace plus {
 
       hide(id_wvobj: string | plus.webview.WebviewObject, aniClose?: plus.webview.AnimationTypeClose, duration?: number, extras?: any): void;
 
-      open(url, id, styles, aniShow, duration, showedCB);
+      /**
+       * 创建并打开Webview窗口
+       * 创建并显示Webview窗口，用于加载新的HTML页面，可通过styles设置Webview窗口的样式，创建完成后自动将Webview窗口显示出来。
+       * @param url 打开窗口加载的HTML页面地址 新打开Webview窗口要加载的HTML页面地址，可支持本地地址和网络地址。
+       * @param id 打开窗口的标识 窗口标识可用于在其它页面中通过getWebviewById来查找指定的窗口，为了保持窗口标识的唯一性，应该避免使用相同的标识来创建多个Webview窗口。 如果传入无效的字符串则使用url参数作为WebviewObject窗口的id值。
+       * @param styles 创建Webview窗口的样式（如窗口宽、高、位置等信息）
+       * @param aniShow 显示Webview窗口的动画效果 如果没有指定窗口动画，则使用默认无动画效果“none”。
+       * @param duration 显示Webview窗口动画的持续时间 单位为ms，默认值为200ms（毫秒）。
+       * @param showedCB Webview窗口显示完成的回调函数 当指定Webview窗口显示动画执行完毕时触发回调函数，窗口无动画效果（如"none"动画效果）时也会触发此回调。
+       * @returns WebviewObject窗口对象
+       */
+      open(url?: string, id?: string, styles?, aniShow?, duration?: number, showedCB?): plus.webview.WebviewObject;
 
-      prefetchURL(url);
+      prefetchURL(url: string);
 
-      prefetchURLs(urls);
+      prefetchURLs(urls: string[]);
 
-      show(id_wvobj, aniShow, duration, showedCB, extras);
+      show(id_wvobj: string | plus.webview.WebviewObject, aniShow?, duration?: number, showedCB?, extras?);
 
       startAnimation(options, otherOptions, callback);
 
@@ -65,34 +85,5 @@ declare namespace plus {
 declare namespace plus.webview {
   type AnimationTypeClose = "auto" | "none" | "slide-out-right";
 
-  interface WebviewObject {
-    id: string;
 
-    /**
-     * 获取当前Webview窗口的创建者
-     */
-    opener(): WebviewObject;
-
-    show(aniShow?: any, duration?: number, showedCB?: Function, extras?: any): void;
-
-    /**
-     * 隐藏Webview窗口可保存已加载HTML页面的上下文数据，能降低应用使用的系统资源，通过show方法可将隐藏的Webview窗口显示出来。
-     * @param aniHide
-     * @param duration
-     * @param extras
-     */
-    hide(aniHide?: AnimationTypeClose, duration?: number, extras?: any): void;
-
-    /**
-     * 关闭并销毁Webview窗口，可设置关闭动画和动画持续时间。
-     * @param aniClose 关闭Webview窗口动画效果
-     * @param duration 关闭Webview窗口的动画持续时间
-     * @param extras 关闭Webview窗口扩展参数
-     */
-    close(aniClose?: AnimationTypeClose, duration?: number, extras?: any): void;
-
-    evalJS(js: string): void;
-
-    [key: string]: any;
-  }
 }
